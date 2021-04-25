6bad99793df3572eeefcd2e814c76abc90ed950a
resufer <66846932+resufer@users.noreply.github.com>
2021-04-25 23:38:31 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Main from './Main/Main';
+
+function App() {
+  return (
+    <div className='general'>
+      <Header />
+      <hr />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
